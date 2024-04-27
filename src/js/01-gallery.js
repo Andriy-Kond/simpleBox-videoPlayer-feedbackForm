9b30c161fab0863ac3f4c5cabf5a6078dfdd04f3
Andrii <andrii.kondratovich@gmail.com>
2024-04-27 08:15:53 +0300

    solve: task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// Change code below this line
+const refs = {
+  body: document.querySelector("body"),
+  gallery: document.querySelector(".gallery"),
+};
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+              <img 
+                class="gallery__image" 
+                src="${preview}"
+                alt="${description}" />
+          </a>
+        </li>`;
+    })
+    .join("");
+}
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "top",
+});
